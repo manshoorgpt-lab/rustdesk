2e76471fafe3e54bdcef6de182cbc3ff8ec2c76c
manshoorgpt-lab <manshoorgpt@gmail.com>
2026-05-12 12:12:31 +0330

    Update external_config.dart

--- flutter/lib/external_config.dart
+++ flutter/lib/external_config.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'common.dart';
+import 'package:flutter_hbb/generated_bridge.dart';
 
 class ExternalConfigLoader {
   static const String CONFIG_FOLDER = 'rust';
