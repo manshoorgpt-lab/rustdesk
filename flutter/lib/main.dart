b288fe9f3712195db110e3397b53d37cb1529976
manshoorgpt-lab <manshoorgpt@gmail.com>
2026-05-12 11:06:49 +0330

    Update main.dart

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -28,6 +28,7 @@ import 'consts.dart';
 import 'mobile/pages/home_page.dart';
 import 'mobile/pages/server_page.dart';
 import 'models/platform_model.dart';
+import 'external_config.dart';
 
 import 'package:flutter_hbb/plugin/handlers.dart'
     if (dart.library.html) 'package:flutter_hbb/web/plugin/handlers.dart';
