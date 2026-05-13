a0852099d09a65cacb0b2f896d49db94737bfda7
manshoorgpt-lab <manshoorgpt@gmail.com>
2026-05-13 07:52:01 +0330

    Update external_config.dart

--- flutter/lib/external_config.dart
+++ flutter/lib/external_config.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'common.dart';
 import 'package:flutter_hbb/generated_bridge.dart';
-import 'package:your_package_name/generated_bridge.dart';
 
 
 class ExternalConfigLoader {
